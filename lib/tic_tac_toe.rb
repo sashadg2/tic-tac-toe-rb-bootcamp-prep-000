4fe82d221948a89033f95794fc8167a52b155f13
Oleksandr Diuriagin <sashadg@gmail.com>
2018-03-04 04:04:16 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -49,7 +49,7 @@ end
 def turn_count(board)
   count = 0
   board.each do |element|
-    if element != "" && element != " " && element != nile
+    if element != "" && element != " " && element != nil
       count = count + 1
     end
 end
